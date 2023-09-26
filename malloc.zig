70149639dbeb895fbec7aeb567cf707fe3e62919
Alif Fatul <aliffatul86tf@gmail.com>
2023-09-27 03:50:44 +0700

    GPA

--- malloc.zig
+++ malloc.zig
@@ -4,10 +4,11 @@ test "stack" {
     var name: [4]u8 = .{ 'a', 'l', 'i', 'p' };
     name[3] = 'f';
 
-    try std.testing.expectEqual([_]u8{ 'a', 'l', 'i', 'f' }, name);
+    try std.testing.expectEqual([4]u8{ 'a', 'l', 'i', 'f' }, name);
+    try std.testing.expectEqualStrings("alif", &name);
 }
 
-test "page_allocator" {
+test "page allocator" {
     const allocator = std.heap.page_allocator;
     var array = try std.ArrayList(u8).initCapacity(allocator, 4);
     defer array.deinit();
@@ -19,11 +20,11 @@ test "page_allocator" {
 }
 
 // example of memory allocation without using defer.
-test "area_allocator" {
+test "area allocator" {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const allocator = arena.allocator();
 
-    var map = std.StringHashMap([]const u8).init(allocator);
+    var map = std.StringArrayHashMap([]const u8).init(allocator);
 
     try map.put("foo", "bar");
     try map.put("bar", "foo");
@@ -33,4 +34,25 @@ test "area_allocator" {
 
     map.deinit();
     arena.deinit();
+}
+
+test "general purpose allocator" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const deinit_status = gpa.deinit();
+        if (deinit_status == .leak) {
+            @panic("TEST FAIL");
+        }
+    }
+
+    const allocator = gpa.allocator();
+
+    var map = std.StringArrayHashMap([]const u8).init(allocator);
+    defer map.deinit();
+
+    try map.put("foo", "bar");
+    try map.put("bar", "foo");
+
+    try std.testing.expectEqualStrings("bar", map.get("foo").?);
+    try std.testing.expectEqualStrings("foo", map.get("bar").?);
 }
